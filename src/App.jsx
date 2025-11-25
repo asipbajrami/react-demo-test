3dd80cbeb9822bb1594b24ad1818614127073c52
Asip Bajrami <75222679+asipbajrami@users.noreply.github.com>
2025-11-25 12:15:14 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
         <p>
-          Endow 2026
+          Endow 2027
         </p>
       </div>
       <p className="read-the-docs">
